fbf519ad39053a479ab3b2f4d3a5883fceb340ea
AwaisAli-17 <awais.ali1312@gmail.com>
2025-01-20 01:36:46 +0200

    Merger Res

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,10 +5,16 @@ import styles from "../styles/main.module.css";
 
 const Main = () => {
   return (
-    <div className={styles.row1_parent_div}>
-      <CurrentWeather />
-      <TodaysHighlight />
-    </div>
+    <>
+      <div className={styles.row1_parent_div}>
+        <CurrentWeather />
+        <TodaysHighlight />
+      </div>
+      <div className={styles.row1_parent_div}>
+        <HourlyForecast />
+        <DailyForecast />
+      </div>
+    </>
   );
 };
 
